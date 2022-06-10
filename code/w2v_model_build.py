0660d42bd2b0b823a4339ab55dd203d09c6bb4ae
mglee <mglee815@postech.ac.kr>
2022-06-10 15:44:28 +0900

    w2v similar word

--- code/w2v_model_build.py
+++ code/w2v_model_build.py
@@ -7,5 +7,5 @@ from gensim.models import Word2Vec
 model = Word2Vec(sentences=key_lst, window=15, min_count=5, workers=8, sg=0)
 
 from gensim.models import KeyedVectors
-model.wv.save_word2vec_format('../result/w2v_model/sample_w2v') # 모델 저장
+model.wv.save_word2vec_format('../result/w2v_model/sample2_w2v') # 모델 저장
 #loaded_model = KeyedVectors.load_word2vec_format('../result/w2v_model/sample_w2v')
