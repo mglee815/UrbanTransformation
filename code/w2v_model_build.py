4f0abad0d229a021a0bdf41c77245b5619ddb6c2
mglee <mglee815@postech.ac.kr>
2022-06-16 19:07:07 +0900

    w2v develop[

--- code/w2v_model_build.py
+++ code/w2v_model_build.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
+print("start")
 DATA_PATH = "../data/pkl/"
+data1 = pd.read_pickle(f"{DATA_PATH}splited1.pkl")
+data2 = pd.read_pickle(f"{DATA_PATH}splited2.pkl")
+print("loaded")
 
-data = pd.read_pickle(f"{DATA_PATH}merged_filtered.pkl")
-key_lst = data['키워드'].apply(lambda x : x.split(','))
+data1
+#===========================================================#
+##############키워드 수가 너무 많아서 특성 키워드로만 해보기############
 
-from gensim.models import Word2Vec
-model = Word2Vec(sentences=key_lst, window=30, min_count=10, workers=8, sg=0)
+# from gensim.models import Word2Vec
+# model = Word2Vec(sentences=key_lst, window=5, min_count=50, workers=8, sg=0)
 
-from gensim.models import KeyedVectors
-model.wv.save_word2vec_format('../result/w2v_model/total_w2v_w30m10') # 모델 저장
-#loaded_model = KeyedVectors.load_word2vec_format('../result/w2v_model/sample_w2v')
+# from gensim.models import KeyedVectors
+# model.wv.save_word2vec_format('../result/w2v_model/total_w2v_w30m10') # 모델 저장
+# #loaded_model = KeyedVectors.load_word2vec_format('../result/w2v_model/sample_w2v')
