b574a175750cbbf2d07cca06b53a32159d601295
mglee <mglee815@postech.ac.kr>
2022-06-21 10:32:41 +0900

    0621 / w2v_by period / keyword count by year / freq by city

--- code/featkey_w2v_by_year.py
+++ code/featkey_w2v_by_year.py
@@ -4,6 +4,9 @@ import datetime
 w = int(input("window size: "))
 m = int(input("min size: "))
 
+start = int(input("from : "))
+end = int(input("to : "))
+
 start_time = datetime.datetime.now()
 print("data load start")
 DATA_PATH = "../data/pkl/"
@@ -13,8 +16,20 @@ print("loaded")
 print(datetime.datetime.now() - start_time)
 
 
-#kileed 됨
-#key_lst = data['키워드'].apply(lambda x : x.split(','))
+y = []
+def str_to_dt(x):
+    temp = datetime.datetime.strptime(str(x), '%Y%m%d')
+    y.append(temp.year)
+
+data['일자'].apply(lambda x : str_to_dt(x))
+data['year'] = y
+
+
+
+temp = data[data['year'] >= start]
+temp.reset_index(inplace=True, drop = True)
+data = temp[temp['year'] <= end]
+print(data.info())
 
 start_time = datetime.datetime.now()
 print('split start')
@@ -25,11 +40,18 @@ print("splited")
 print(datetime.datetime.now() - start_time)
 
 
+
+#for idx, txt in enumerate(list(data['특성추출(가중치순 상위 50개)'])):
+#     if type(txt) == float:
+#         data.iloc[idx, 15] = "NONE"
+
+
 print('model build')
 from gensim.models import Word2Vec
 model = Word2Vec(sentences=key_lst, window=w, min_count=m, workers=8, sg=0)
 
 from gensim.models import KeyedVectors
-model.save(f'../result/w2v_model/{datetime.datetime.now().strftime("%m_%d_%H")}_w2v_w{w}m{m}') # 모델 저장
-print('model saved')
+MODEL_NAME = f'{datetime.datetime.now().strftime("%m_%d_%H")}_w2v_{start}_{end}_w{w}m{m}'
+model.save(f'../result/w2v_model/{MODEL_NAME}') # 모델 저장
+print(f'model saved  as {MODEL_NAME}')
 print(datetime.datetime.now() - start_time)
